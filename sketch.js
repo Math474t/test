59a690021ce411782313ecbca0f44253b5187e2e
Math474t <94458405+Math474t@users.noreply.github.com>
2023-09-20 08:58:53 +0200

    ball no bounce

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
 let x = 0;
+let y = 0;
 let w = 100;
 function setup() {
-  createCanvas(400, 400);
-
+  createCanvas(window.innerWidth, window.innerHeight);
+  
 }
 
 function draw() {
   background(220);
   fill(255, 255, 255);
-  circle(x, 100, w, 200);
+  circle(x, y, w);
   x = x + 10;
-  if (x > width-w) {
+  y = y + 10;
+  if (x > width) {
     x = 0;
+  }
+  if (y > height) {
+    y = y - 10;
+  }
+  if (y < 0) {
+    y = y + 10;
   }
 }
